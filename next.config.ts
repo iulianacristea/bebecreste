01e9b566866de29cda511e7747b5c817603df245
iulianacristea <61791652+iulianacristea@users.noreply.github.com>
2026-05-07 21:33:10 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -5,26 +5,6 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: path.resolve(__dirname),
   },
-  async redirects() {
-    return [
-      {
-        source: "/home",
-        destination: "https://bebecreste.ro",
-        permanent: true,
-      },
-      {
-        source: "/:path*",
-        destination: "https://bebecreste.ro/:path*",
-        permanent: true,
-        has: [
-          {
-            type: "host",
-            value: "www.bebecreste.ro",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
